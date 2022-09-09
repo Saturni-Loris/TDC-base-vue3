7f8d46b9248e65211038f4a3f02d2858d3ec255d
Saturni-Loris <loris.saturni@edu.univ-fcomte.fr>
2022-09-09 10:47:25 +0200

    offre maison

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,22 @@
 <template>
-  <nav>
-    <h4 class="text-xl">
-      <Bars3Icon class="inline-block h-5 w-5 text-blue-500" />
-      menu (dans <code class="font-mono">/src/App.vue</code>)
-    </h4>
-    <ul>
-      <li>
-        <router-link class="text-red-600 underline" to="/">
-          lien vers
-          <code class="font-mono">/src/pages/index.vue</code>
-        </router-link>
-      </li>
-    </ul>
-  </nav>
+  <div>
+    <nav>
+      <h4 class="text-xl">
+        <Bars3Icon class="inline-block h-5 w-5 text-blue-500" />
+        menu (dans <code class="font-mono">/src/App.vue</code>)
+      </h4>
+      <ul>
+        <li>
+          <router-link class="text-red-600 underline" to="/">
+            Accueil
+          </router-link>
+          <router-link class="text-red-600 underline" to="simple">
+            Simple
+          </router-link>
+        </li>
+      </ul>
+    </nav>
+  </div>
 
   <!-- Affiche les pages -->
   <router-view class="m-2 border-2 p-2" />
---
+++ src/pages/simple.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="p-2">
+        <h2>Page simple</h2>
+    </div>
+</template>
